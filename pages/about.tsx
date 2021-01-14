52afa33efb22b508159de3c4032af247f9fd8c95
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2021-01-14 17:31:29 +0600

    sass module

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/sassstyle.module.scss';
+
+
+
+
+function About(){
+    return (
+        <div>
+            <h2 className={styles.h1}>This is from about page using sass</h2>
+        </div>
+    );
+}
+
+export default About;
