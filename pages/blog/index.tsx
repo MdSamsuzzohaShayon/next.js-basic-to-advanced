c47239036f37c67b36cca94f58a5de92b27db45e
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2021-01-14 17:43:48 +0600

    file structure

---
+++ pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+const index = () => {
+    return (
+        <div className={styles.wrapper}>
+            <h2 className={styles.pm}>This is blog</h2>
+            <h1 className={styles.pm}>This is blog</h1>
+        </div>
+    )
+}
+
+export default index;
