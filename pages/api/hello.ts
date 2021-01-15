614a75c17838f4cf3a35dd0d4a90a06020c1a1d4
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2021-01-15 13:52:45 +0600

    next.js api route support

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Any file inside the folder pages/api is mapped to /api/* and will be treated as an API endpoint instead of a page. They are server-side only bundles and won't increase your client-side bundle size.
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req:NextApiRequest, res:NextApiResponse) => {
+
+  res.statusCode = 200;
+  res.json({ status: 'Ok' });
 }
+
+
+
+// http://localhost:3000/api/hello
