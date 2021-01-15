81922231a028912a45258cc2ca7f8ac20d909ecf
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2021-01-15 16:54:34 +0600

    react hook modern form submittion

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,13 +1,53 @@
 import styles from '../styles/auth.module.scss';
+import { useState } from 'react';
+import jwt from 'jsonwebtoken';
 
 const auth = () => {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [message, setMessage] = useState<string>('You are not logged in');
+
+
+    async function submitForm(e) {
+        e.preventDefault();
+        // console.log("On Submit button hit");
+        
+        const res = await fetch('/api/login', {
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({ username, password })
+        }).then(t => t.json());
+        const token = res.token;
+        console.log(res.token);
+        
+        if(token){
+            const json =jwt.decode(token) as {[key: string]: string};
+            console.log(json);
+            
+            setMessage(`Welcome ${json.username} and you are ${json.admin ? " an admin" : " not an admin"}` );
+        }else{
+            setMessage("Something went wrong");
+        }
+    }
     return (
         <div className={styles.wrapper}>
-            <h2>Log In </h2>
-            <form className={styles.form} action="/api/login" method="POST">
-                <input className={styles.input} name="username" type="text" defaultValue='admin' placeholder="Enter your username" />
+            <h2>{message}</h2>
+            <form className={styles.form} onSubmit={submitForm} >
+                <input
+                    onChange={e => setUsername(e.target.value)}
+                    className={styles.input}
+                    type="text"
+                    value={username}
+                    placeholder="Enter your username"
+                />
                 <br />
-                <input className={styles.input} name="password" type="password" defaultValue='admin' placeholder="Enter your password" />
+                <input
+                    onChange={e => setPassword(e.target.value)}
+                    className={styles.input}
+                    value={password}
+                    type="password"
+                    placeholder="Enter your password"
+                />
                 <br />
                 <input className={styles.submit} type="submit" value="submit" />
             </form>
