d5cef992f810f6729ded863d46f56d64ec6067e0
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2021-01-15 17:10:27 +0600

    json web token

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,32 +6,48 @@ const auth = () => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [message, setMessage] = useState<string>('You are not logged in');
+    const [secret, setSecret] = useState<string>('');
 
 
     async function submitForm(e) {
         e.preventDefault();
         // console.log("On Submit button hit");
-        
+
         const res = await fetch('/api/login', {
             method: "POST",
-            headers: {"Content-Type":"application/json"},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
         }).then(t => t.json());
         const token = res.token;
         console.log(res.token);
-        
-        if(token){
-            const json =jwt.decode(token) as {[key: string]: string};
+
+        if (token) {
+            const json = jwt.decode(token) as { [key: string]: string };
             console.log(json);
-            
-            setMessage(`Welcome ${json.username} and you are ${json.admin ? " an admin" : " not an admin"}` );
-        }else{
+
+            setMessage(`Welcome ${json.username} and you are ${json.admin ? " an admin" : " not an admin"}`);
+
+            const res = await fetch('/api/secret', {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ token })
+            }).then(t => t.json());
+
+            if(res.secretAdminCode){
+                setSecret(res.secretAdminCode);
+            }else{
+                setSecret("Nothing availabe");
+            }
+
+
+        } else {
             setMessage("Something went wrong");
         }
     }
     return (
         <div className={styles.wrapper}>
             <h2>{message}</h2>
+            <h5>Secret: {secret}</h5>
             <form className={styles.form} onSubmit={submitForm} >
                 <input
                     onChange={e => setUsername(e.target.value)}
